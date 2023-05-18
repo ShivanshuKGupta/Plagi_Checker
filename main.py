d33b71a94f9d586a0250d9c2d7b5fe3e6ca0d9d9
Shivanshu Gupta <shivanshukgupta@gmail.com>
2023-05-18 23:10:40 +0530

    Additional features added

---
+++ main.py
@@ -0,0 +1,40 @@
+import shutil
+import os
+from DolosAPI.dolos import dolos
+
+
+if (shutil.which("npm") is None):  # dolos is not installed then
+    print("npm is not installed. Please install node from https://nodejs.org/ then run this script.")
+    exit(0)
+
+
+if (shutil.which("dolos") is None):  # dolos is not installed then
+    if (input("Dolos is not installed. Would you like to install it now? (y/n)").lower() == 'y'):
+        os.system("npm install -g @dodona/dolos")
+    else:
+        print("Exiting...")
+        exit(0)
+
+
+print("How to use?")
+print("Create a folder let us say 'submissions' copy all .c/.cpp files there.")
+print("Finally just enter that folder path down here.")
+
+path = input("Enter the submissions folder path:")
+
+with open("inputFiles.csv", "w") as file:
+    file.write("filename"+'\n')
+    for each_input_file in os.listdir(path):
+        file.write(path + "\\"+each_input_file+'\n')
+
+
+dolos.output_format = "csv"
+dolos.output_destination = f"Dolos_Reports"
+dolos.path = f"inputFiles.csv"
+dolos.language = "cpp"
+dolos.min_similarity = 0.5
+dolos.sort_by = "similarity"
+dolos.run()
+print(f"See generated report here: {os.getcwd()}\\Dolos_Reports\\pairs.csv")
+
+os.remove('inputFiles.csv')
